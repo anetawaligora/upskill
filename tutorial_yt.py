31e517d86d87851c5a797b4f6b5c79374c82379a
anetawaligora <aneta.waligora94@gmail.com>
2022-07-21 21:53:51 +0200

    Learning from youtube tutorial

---
+++ tutorial_yt.py
@@ -0,0 +1,37 @@
+# Episode2 Variables - Strings
+
+message = 'Hello world'
+print(message)
+
+message_a = "Hello\' world"
+print(message_a)
+
+message_b = """Hello world
+welcome to the jungle"""
+print(message_b)
+
+# Concatenation
+greetings = 'Hello'
+name = 'Aneta'
+mail = greetings + ' ' + name
+print(mail)
+
+# function len
+message_c = "How long is lenght my message"
+print(message_c)
+print(len(message_c))
+
+print(len(message))
+print(message[6:])
+
+# Lower and upper case
+print(message.lower())
+print(message.upper())
+
+print(message.find('ello'))
+
+# dir - method and atributes avaiable for variable type
+print(dir(message))
+
+# Episode 3 Variables - Integers and Floats
+
