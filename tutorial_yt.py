6597e6e6788f810dad324fdffae4fd191e205f8b
anetawaligora <aneta.waligora94@gmail.com>
2022-07-27 23:14:15 +0200

    Adding learn of python function

--- tutorial_yt.py
+++ tutorial_yt.py
@@ -234,12 +234,57 @@ greetings()
 personal_greeting("Aneta", "girl")
 
 
-# def sum():
-#    print("sum=")
+def items(summant_1, summant_2):
+    print("sum=" + ' ' + str(summant_1 + summant_2))
 
 
-#def items(summant_1, summant_2):
-    #print("sum=" + ' ' + summant_1 + summant_2)
+items(3, 2)
 
 
-#items(3, 2)
+def hello_func():
+    pass
+
+
+print(hello_func())
+
+
+def hello_func():
+    return ('Hello!')
+
+
+hello_func()
+hello_func()
+
+print(hello_func().upper())
+
+
+def student_info(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+student_info('Math', 'Art', name='John', age=22)
+
+month_days = [0, 31, 28, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def is_leap(year):
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
+def days_in_month(year, month):
+    if not 1 <= month <= 12:
+        return 'Invalid'
+
+    if month == 2 and is_leap(year):
+        return 29
+
+    return month_days[month]
+
+
+print(days_in_month(2020, 12))
+
+print(is_leap(2020))
+print(is_leap(2022))
+
+#Episode 9 Import Modules and Exploring the Standard Library
