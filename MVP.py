d7095212505e1c110db6bd069937d5eedb2629aa
anetawaligora <aneta.waligora94@gmail.com>
2022-08-10 00:00:46 +0200

    Adding input with the number of draws, input with the choice if whether to still draw and condition for drawing without weighted in JSON format

--- MVP.py
+++ MVP.py
@@ -15,6 +15,8 @@ print(random.choice(participants))
 
 # I need to import json file participant1.json - in this file are participants data in json format without weighted
 # I need to draw winnings
+# User should be able to add input with number of draws
+# User should be able to chose if he wants to continue the draw
 
 import json
 
@@ -23,16 +25,23 @@ with open("data/participants1.json") as jsonFile:
     jsonFile.close()
 
 print(jsonObject)
-print(random.choice(jsonObject))
+print("How many times to make a draw?")
+x = int(input())
+drawings = (random.choices(jsonObject, k=x))
+drawing = (random.choice(jsonObject))
 
-#I need to import csv file participants2.csv
-#I need to draw winings considering the weights
-#The grates weight should be drawing the greates number of times
+print(drawings)
+selection = input("Do you want to continue to draw winners? Answer Yes or No ")
+if selection == "Yes":
+    print(drawing)
+elif selection == "No":
+    print("End of draw")
 
-with open("data/participants2.csv") as csvfile:
-    participants2_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    participants = list(participants2_reader)[1:]
-print(participants)
+# I need to import csv file participants2.csv
+# I need to draw winings considering the weights
+# The grates weight should be drawing the greates number of times
 
-import numpy as np
-
+# with open("data/participants2.csv") as csvfile:
+#    participants2_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+#    participants = list(participants2_reader)[1:]
+# print(participants)
